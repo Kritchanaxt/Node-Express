21521374851921fba4d621f2a7fa54b19886bbe9
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2024-03-28 16:52:59 +0700

    Update 8081.js

--- Node.js/Basic/Webserver/8081.js
+++ Node.js/Basic/Webserver/8081.js
@@ -1,18 +1,45 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    const myhtml = `
+    const pathName = req.url;
+    console.log("url = ", pathName);
+    if(pathName === "/" || pathName === "/home"){
+        const myhtml = `
         <style>
             body {
                 background-color: lightblue;
             }
+            h1 {
+                color: green;
+            }
             p {
-                color: red;
+                color: blue;
             }
         </style>
-        <h1>Hello Node.js</h1>
+        <h1>Hello Homepage</h1>
         <p>Kritchanat student | 2024</p>`;
     res.end(myhtml);
+    } else if (pathName === "/product"){
+        const myhtml = `
+        <style>
+            p {
+                color: yellow;
+            }
+        </style>
+        <h1>Hello Product</h1>
+        <p>ice-cream yummy</p>`;
+    res.end(myhtml);
+    } else {
+        res.writeHead(404);
+        const myhtml = `
+        <style>
+            h1 {
+              color: red;
+            }
+        </style>
+        <h1>Not Found</h1>`;
+    res.end(myhtml);
+    }
 });
 
 server.listen(8081, 'localhost', () => {
