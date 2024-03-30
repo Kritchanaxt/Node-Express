f814b05c4749945530df6acb020cdfb85b797a99
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2024-03-30 16:09:49 +0700

    Update myRouter.js

--- Node.js/Basic/Websever/myRouter.js
+++ Node.js/Basic/Websever/myRouter.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-router.get("/", (req, res) => {
+router.get("/", (req,res) => {
     res.status(200)
     res.type('text/html')
-    res.sendFile(path.join(__dirname, '..', 'webpage', 'index.html'))
+    res.sendFile(path.join(__dirname, 'webpage', 'index.html'))
 })
 
 router.get("/product",(req,res)=>{
-    res.sendFile(path.join(__dirname, '..', 'webpage', 'product.html'))
+    res.sendFile(path.join(__dirname, 'webpage', 'product.html'))
 })
 
 router.use((req, res) => {
